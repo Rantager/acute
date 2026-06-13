eaeb5e6efa69bd52dd415590480339c162e785c6
luizzz16 <lmedina9@ucol.mx>
2026-06-13 14:54:17 -0600

    fix supabase ws 1.2

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,18 @@
-// import tailwind from '@astrojs/tailwind';
 
-// export default {
+
+// import { defineConfig } from 'astro/config';
+// import tailwind from '@astrojs/tailwind';
+// import vercel from '@astrojs/vercel/serverless';
+
+// // https://astro.build/config
+// export default defineConfig({
 //   integrations: [tailwind()],
-//   site: 'https://www.acute.dev',
-//   markdown: {
-//     syntaxHighlight: 'shiki'
-//   }
-// };
+//   output: 'hybrid', // Mantiene tu web comercial estática
+//   adapter: vercel({
+//     // Forzamos quirúrgicamente a que las Serverless Functions se compilen para Node 20
+//     runtime: 'nodejs20.x'
+//   }),
+// });
 
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
@@ -20,4 +26,11 @@ export default defineConfig({
     // Forzamos quirúrgicamente a que las Serverless Functions se compilen para Node 20
     runtime: 'nodejs20.x'
   }),
+  
+  // Agregamos esta sección para ganarle la batalla a Vite y Vercel
+  vite: {
+    ssr: {
+      external: ['ws'] // Le dice a Vite que no toque 'ws' en el cliente, y a Vercel que lo instale en Node 20
+    }
+  }
 });
